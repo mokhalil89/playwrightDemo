466031906e9948604dbb420c842a2698e595bbd9
mokhalil89 <mohamedkhalilmahmoud89@gmail.com>
2022-03-13 10:17:13 +0100

    update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -20,32 +20,89 @@ test('Click on Elements', async ({ page }) => {
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
 
-test.skip('Selectors ', async ({ page }) => {
-  //text
-  await page.click('text=some text')
+// test.skip('Selectors ', async ({ page }) => {
+//   //text
+//   await page.click('text=some text')
 
-  //css selectors
-  await page.click('button')
-  await page.click('#id')
-  await page.click('.class')
+//   //css selectors
+//   await page.click('button')
+//   await page.click('#id')
+//   await page.click('.class')
 
-  //only visible css selectors
-  await page.click('.submit-button:visible')
+//   //only visible css selectors
+//   await page.click('.submit-button:visible')
 
-  //combinations
-  await page.click('#username .first')
+//   //combinations
+//   await page.click('#username .first')
 
-  //xpath
-  await page.click('//button')
-})
+//   //xpath
+//   await page.click('//button')
+// })
 
-test('Working with Inputs', async ({ page }) => {
-  await page.goto('http://zero.webappsecurity.com/index.html')
-  await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
-  await page.click('#signin_button')
-  await page.type('#user_login', 'some username')
-  await page.type('#user_password', 'some password')
-  await page.click('text=sign in')
-  const errorMessage = await page.locator('.alert-error')
-  await expect(errorMessage).toContainText('Login and/or password are wrong.')
+test.describe('My first test suite', () => {
+  test('simple basic test @run', async ({ page }) => {
+    await page.goto('https://www.checklyhq.com/')
+    const pageTitle = await page.locator('h1')
+    await expect(pageTitle).toContainText(
+      'Delightful Active Monitoringfor Developers'
+    )
+    await page.click(
+      "div[class='optanon-alert-box-button optanon-button-allow'] div[class='optanon-alert-box-button-middle']"
+    )
+    await page.click('#login-button')
+  })
+
+  test('Click on Elements @run', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/')
+    await page.click('#signin_button')
+    await page.click('text=sign in')
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  })
+
+//   test.skip('Selectors ', async ({ page }) => {
+//     //text
+//     await page.click('text=some text')
+
+//     //css selectors
+//     await page.click('button')
+//     await page.click('#id')
+//     await page.click('.class')
+
+//     //only visible css selectors
+//     await page.click('.submit-button:visible')
+
+//     //combinations
+//     await page.click('#username .first')
+
+//     //xpath
+//     await page.click('//button')
+//   })
+  //.only to run the specified test
+  test('Working with Inputs', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await expect(page).toHaveURL('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.type('#user_login', 'some username')
+    await page.type('#user_password', 'some password')
+    await page.click('text=sign in')
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+  })
+
+//   test.skip('Assertions', async ({ page }) => {
+//     await page.goto('')
+//     await expect(page).toHaveURL('')
+//     await expect(page).toHaveTitle('')
+
+//     const element = await page.locator('')
+//     await expect(element).toBeVisible()
+//     await expect(element).toHaveText('')
+//     await expect(element).toHaveCount(1)
+
+//     const nonExistingElement = await page.locator('')
+//     await expect(nonExistingElement).not.toBeVisible()
+//   })
+
+//   test('Annotation', async ({ page }) => {})
 })
